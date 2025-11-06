61549a0105d8eaea63e4019cd7c986175d0e3763
NoelBDavalos <noel.davalos@uabc.edu.mx>
2025-11-06 19:45:23 +0000

    11:45

--- P9/p9.asm
+++ P9/p9.asm
@@ -5,6 +5,7 @@ N equ 5
 section .data
     V1 db 1,2,3,4,5
     V2 db 1,2,3,4,5
+    P_Escalar dw 0
 
 section .text
     global _start
@@ -21,15 +22,24 @@ _start:
     mov ecx, N
     mov ebx, V1
     mov edx, V2
+    .loop_P_Escalar:
+        mov al, [ebx]
+        mul al, [edx]
+        add al, [P_Escalar]
+        mov [P_Escalar], ax
+        inc ebx
+        inc edx 
+
+    mov ecx, N
+    mov ebx, V1
+    mov edx, V2
     call Sumar
 
     mov ecx, N
     mov ebx, V1
     call Desplegar
 
-
-; Fin del programa
-
+    ; Fin del programa
     mov eax, 1              ;system call number (sys_exit) -- fin del programa
 	int 0x80                ;call kernel
 
